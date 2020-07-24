6d32143627a4014492f2c64fa4eacaed15deb2d6
Alfonso Delgado <alfonso.delgado@hiumanlab.com>
2020-07-24 15:40:20 -0500

    docs agregado

---
+++ docs/precache-manifest.f432d899ec14aee271da4c3894467c51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1609d28ad82dded8c6aa9c211a4a90f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0403e3cd19499acc649c",
+    "url": "/static/css/main.eee8e862.chunk.css"
+  },
+  {
+    "revision": "92a5aaf936af7a3a2930",
+    "url": "/static/js/2.7c8d2b56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7c8d2b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0403e3cd19499acc649c",
+    "url": "/static/js/main.7faf4063.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
